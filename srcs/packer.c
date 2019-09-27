783574de98b8c7cb5f4c985a6fb1536f44f7f821
john <jandreu@student.42.fr>
2019-09-27 09:03:57 -0500

    Commit de la honte etat chelou et code degeu

--- srcs/packer.c
+++ srcs/packer.c
@@ -31,6 +31,8 @@ void	parse_pheader(t_info info)
 	uint32_t	new_entry;
 	uint32_t	woody_size = ((char *)&print_woody_end - (char *)&print_woody);
 	uint32_t	decipher_size = (char *)&end_decipher - (char *)&decipher;
+	uint32_t	alignement;
+	int 		text_done = 0;
 	
 
 	
@@ -42,16 +44,24 @@ void	parse_pheader(t_info info)
 		if (pheader[i].p_type == PT_LOAD && pheader[i].p_vaddr != 0)
 		{
 			debug("Found load and execute segment\n");
+			debug("V ADDR != 0\n");
 			if (i + 1 < header->e_phnum)
 			{
+				if (pheader[i].p_flags != (PF_R + PF_X) && text_done == 0)
+				{
+					continue ;
+				}
+				text_done = 1;
+
 				printf("Vaddr : %x | Size : %x\n", pheader[i].p_vaddr, pheader[i].p_filesz);
 				cave_size = pheader[i + 1].p_vaddr - (pheader[i].p_vaddr + pheader[i].p_filesz);
 				printf("Taille de la cave : %x (%d)\n", cave_size, cave_size);
-				if (cave_size > info.exploit_size)
+				if ((int)cave_size > ((int)info.exploit_size + 256))
 				{
 
 					pheader[i].p_flags = (PF_R + PF_X + PF_W);
 					new_entry = pheader[i].p_vaddr + pheader[i].p_memsz;
+					alignement = align(new_entry);
 					new_entry += align(new_entry);
 
 					pheader[i].p_memsz += info.exploit_size + align(woody_size); //exploit size + alignement
@@ -89,7 +99,67 @@ void	parse_pheader(t_info info)
 			{
 				printf("Dernier block\n");
 			}
-			
+		}
+		else if (pheader[i].p_type == PT_LOAD && pheader[i].p_vaddr == 0)
+		{
+			debug("Found load and execute segment\n");
+			debug("V ADDR == 0\n");
+			if (i + 1 < header->e_phnum)
+			{
+				if (pheader[i].p_flags != (PF_R + PF_X) && text_done == 0)
+				{
+					continue ;
+				}
+				text_done = 1;
+				printf("Vaddr : %x | Size : %x\n", pheader[i].p_vaddr, pheader[i].p_filesz);
+				cave_size = pheader[i + 1].p_offset - pheader[i].p_filesz;
+				printf("Taille de la cave : %x (%d)\n", cave_size, cave_size);
+				if ((int)cave_size > ((int)info.exploit_size + 256))
+				{
+
+					pheader[i].p_flags = (PF_R + PF_X + PF_W);
+					new_entry = pheader[i].p_memsz;
+					alignement = align(new_entry);
+					new_entry += align(new_entry);
+
+					// pheader[i].p_memsz += info.exploit_size + align(woody_size); //exploit size + alignement
+					// pheader[i].p_filesz += info.exploit_size + align(woody_size);
+					
+					printf("Total size of segment text : %x\n", pheader[i].p_memsz);
+
+					printf("WOODY OFFSET : %x\n", new_entry);
+					memcpy(info.file + new_entry, (void *)&print_woody, woody_size);
+					
+					printf("OLD ENTRY : %x\n", header->e_entry);
+					fill_asm_var(info.file + new_entry, 'A', woody_size, new_entry - header->e_entry);
+					cipher((void *)info.file + header->e_entry, "ABCDEFGHIJKMLNOP", (new_entry - header->e_entry) + woody_size, info.keystream);
+					
+					new_entry += woody_size;
+					alignement += align(new_entry);
+					new_entry += align(new_entry);
+					printf("Final Entry point (decipher) : %x\n", new_entry);
+					memcpy(info.file + new_entry, (void *)&decipher, decipher_size);
+					memcpy(info.file + new_entry + decipher_size, info.keystream, 256);
+
+					printf("Exploit size : %x (%d)\n", info.exploit_size + 255, info.exploit_size + 255);
+					
+					
+					fill_asm_var(info.file + new_entry, 'C', decipher_size, new_entry - header->e_entry);
+					fill_asm_var(info.file + new_entry, 'Z', decipher_size, woody_size + align(new_entry) + align(woody_size));
+					
+					header->e_entry = new_entry;
+					write(info.new_fd, info.file, info.file_size);
+					return ;
+				}
+				else
+				{
+					printf("Pas assez de place\n");
+				}
+			}
+			else
+			{
+				printf("Dernier block\n");
+			}
 		}
 	}
 	
